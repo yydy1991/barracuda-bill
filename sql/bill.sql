5d62076ea8db1abf3829252bd90a2cc6af9c714f
HM <oncwnuIohB2kU8KBPa_CcXPxend4@git.weixin.qq.com>
2024-10-30 22:14:02 +0800

    add：添加开发环境、部署环境的配置

--- sql/bill.sql
+++ sql/bill.sql
@@ -5,6 +5,20 @@ CREATE TABLE `bi_bill_type` (
                                 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='账单类型表';
 
+INSERT INTO `sys_menu` (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `target`, `menu_type`, `visible`, `is_refresh`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`)
+VALUES
+    (2000, '账单', 0, 5, '#', 'menuItem', 'M', '0', '1', NULL, 'fa fa-credit-card-alt', 'admin', '2024-10-25 23:12:36', '', NULL, '');
+
+INSERT INTO `bi_bill_type` (`id`, `name`, `code`)
+VALUES
+    (1, '餐饮美食', '0001'),
+    (2, '服饰装扮', '0002'),
+    (3, '日用百货', '0003'),
+    (4, '家居家装', '0004'),
+    (5, '数码电器', '0005'),
+    (6, '运动户外', '0006'),
+    (7, '美容美发', '0007');
+
 CREATE TABLE `bi_bill` (
                            `id` int unsigned NOT NULL AUTO_INCREMENT,
                            `create_by` varchar(64) DEFAULT NULL,
--- src/main/java/com/barracuda/barracudabill/BarracudaBillApplication.java
+++ src/main/java/com/barracuda/barracudabill/BarracudaBillApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication(scanBasePackages = {"com.barracuda"}, exclude = {DataSourceAutoConfiguration.class})
 public class BarracudaBillApplication {
     public static void main(String[] args) {
@@ -13,8 +16,14 @@ public class BarracudaBillApplication {
         ConfigurableEnvironment environment = context.getEnvironment();
         Integer serverPort = environment.getProperty("server.port", Integer.class);
         String contextPath = environment.getProperty("server.servlet.context-path", String.class);
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ignored) {
+
+        }
         System.out.println("barracuda bill application started");
-        System.out.printf("http://localhost:%d%s", serverPort, contextPath);
+        System.out.printf("http://%s:%d%s", hostAddress, serverPort, contextPath);
 
     }
 
